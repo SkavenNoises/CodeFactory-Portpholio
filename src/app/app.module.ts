4752fb07a79db700150fad468bcec366835573dd
Aki <kieran.mortimer@gmail.com>
2020-06-28 16:42:53 +0200

    added nested codereviews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { GithubPageComponent } from './components/github-page/github-page.component';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomePageComponent, GithubPageComponent, ProjectsPageComponent],
+  declarations: [AppComponent, NavbarComponent, HomePageComponent, GithubPageComponent, ProjectsPageComponent, JumbotronComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -3,48 +3,48 @@ export const projectList = [
     id: 1,
     src: 'CFLMS-KieranMortimer-CodeReview-01',
     description: 'First Website',
-    thumb: '#',
+    thumb: 'rev1.png',
   },
 
   {
     id: 2,
     src: 'CFLMS-KieranMortimer-CodeReview-02',
     description: 'Online Newspaper',
-    thumb: '#',
+    thumb: 'rev2.png',
   },
 
   {
     id: 3,
     src: 'CFLMS-KieranMortimer-CodeReview-03',
     description: 'Car Insurance Website',
-    thumb: '#',
+    thumb: 'rev3.png',
   },
 
   {
     id: 4,
     src: 'LMSCF-10-CoreTech-8',
     description: 'Team Webpage',
-    thumb: '#',
+    thumb: 'rev4.png',
   },
 
   {
     id: 5,
     src: 'CFLMS-KieranMortimer-CodeReview-05',
     description: 'Film Review Webpage',
-    thumb: '#',
+    thumb: 'rev5.png',
   },
 
   {
     id: 6,
     src: 'CFLMS-KieranMortimer-CodeReview-06',
     description: 'Travel-o-matic Blog',
-    thumb: '#',
+    thumb: 'rev6.png',
   },
 
   {
     id: 7,
     src: 'CFLMS-KieranMortimer-CodeReview-07',
     description: 'Travel Agents website',
-    thumb: '#',
+    thumb: 'rev7.png',
   },
 ];
